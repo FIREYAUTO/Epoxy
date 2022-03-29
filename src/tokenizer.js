f48f7b2f2853d16d3925ed43e9c354538cbf7f8b
FIREYAUTO <56837610+FIREYAUTO@users.noreply.github.com>
2022-03-29 09:21:42 -0700

    Update tokenizer.js

--- src/tokenizer.js
+++ src/tokenizer.js
@@ -21,6 +21,7 @@ class TokenizerStack {
 	Next(Amount=1){
 		this.Index+=Amount;
 		this.Token=this.Tokens[this.Index];
+		if(!this.Token)this.Token=Tokenizer.EOSToken;
 		return this.Token;
 	}
 	IsEnd(){
@@ -82,13 +83,19 @@ class TokenizerStack {
 	}
 	ParseToken(Character){
 		Character = this.ComputePossibleCharacter(Character);
-		let RawToken = EPXTokens.GetFromLiteral(Character);
+		let RawToken = EPXTokens.GetFromRawLiteral(Character);
 		let Token = new BaseToken(Character,Character,"Identifier");
-		if(RawToken){
+		if(RawToken)
 			Token.Name = RawToken.Name,
 				Token.Literal = Character,
 				Token.Type = RawToken.Type;
+		if(Token.Type==="Whitespace"&&Token.LineBreak){
+			this.TokenIndex=0,
+				this.TokenLine++;
 		}
+		this.TokenIndex	+= Character.length;
+		Token.Index=this.TokenIndex,
+			Token.Line=this.TokenLine;
 		return Token;
 	}
 	Parse(){
@@ -97,6 +104,75 @@ class TokenizerStack {
 			if(Token)this.Write(Token);
 			this.Next();
 		}
+		this.Result=this.RemoveWhitespace(this.HandleTokenTypes(this.Result));
+	}
+	//Whitespace Remover
+	RemoveWhitespace(Tokens){
+		let Result=[];
+		for(let Token of Tokens)if(Token.Type!="Whitespace")Result.push(Token);
+		return Result;
+	}
+	//Token Type Handling
+	ParseNumber(Stack,Token){
+		let Value = Token.Literal;
+		if(isNaN(+Value))return [false];
+		let Next = Stack.Next();
+		if(Next.is("DOT","Operator")){
+			let Number = Stack.Next();
+			if(isNaN(+Number.Literal)){Stack.Next(-2);return [false]}
+			Value+="."+Number.Literal;
+		}else{
+			Stack.Next(-1);
+		}
+		let ENext = Stack.Next();
+		if(ENext.is("E","Identifier")||ENext.is("e","Identifier")){
+			let ESuffix = Stack.Next(),
+			    Amount = 2;
+			if(ESuffix.is("ADD","Operator")||ESuffix.is("SUB","Operator"))
+				Value+=ENext.Literal+ESuffix.Literal,
+					ESuffix=Stack.Next(),
+					Amount++;
+			if(isNaN(+ESuffix.Literal)){Stack.Next(-Amount);return [false]}
+			Value+=ESuffix.Literal;
+		}
+		return [true,+Value];
+	},
+	ToNumber(Token,Value){
+		Token.Type="Constant",
+			Token.Name="Number",
+			Token.Literal=Value;
+	}
+	ParseTokenType(Stack,Token){
+		if(Token.isType("Boolean")){
+			Token.Type = "Constant",
+				Token.Literal = Token.Name==="TRUE"?true:false,
+				Token.Name = "Boolean";
+		}else if(Token.isType("Null")){
+			Token.Type = "Constant",
+				Token.Literal = null,
+				Token.Name = "Null";
+		}else if(Token.isType("Identifier")){
+			let [Success,Result] = this.ParseNumber(Stack,Token);
+			if(Success)this.ToNumber(Token,Result);
+		}
+		return Token;
+	},
+	HandleTokenTypes(Tokens){
+		let Stack={
+			Index:0,
+			Tokens:Tokens,
+			Token:Tokens[0],
+			Result:[],
+			IsEnd:function(){return this.Index>=this.Tokens.length},
+			Next:function(Amount=1){this.Index+=Amount;this.Token=this.Tokens[this.Index];return this.Token},
+			Write:function(Token){this.Result.push(Token);return Token},
+		};
+		while(!Stack.IsEnd()){
+			let Result = this.ParseTokenType(Stack,Stack.Token);
+			if(Result)Stack.Write(Result);
+			Stack.Next();
+		}
+		return Stack.Result;
 	}
 }
 
@@ -108,6 +184,7 @@ const Tokenizer = {
 	New:function(...Arguments){
 		return new TokenizerStack(...Arguments);
 	},
+	EOSToken:new InternalToken("EOS","<eos>","None"),
 }
 
 /*************************\
