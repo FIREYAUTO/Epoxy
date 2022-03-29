2017f0126b03b2ddd63a91c83aca22788e734236
FIREYAUTO <56837610+FIREYAUTO@users.noreply.github.com>
2022-03-29 12:29:51 -0700

    Update tokenizer.js

--- src/tokenizer.js
+++ src/tokenizer.js
@@ -2,7 +2,7 @@
           Imports
 \*************************/
 
-import {Tokens as EPXTokens, TokenTypes as EPXTokenTypes, InternalToken, BaseToken} from "/src/tokens.js";
+import {Tokens as EPXTokens, TokenTypes as EPXTokenTypes, InternalToken, BaseToken} from "https://fireyauto.github.io/Epoxy/src/tokens.js";
 
 /*************************\
       Tokenizer Stack
