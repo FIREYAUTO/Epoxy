b80e3ab65cd24dfcde825b844907f0eba1a3979a
FIREYAUTO <56837610+FIREYAUTO@users.noreply.github.com>
2022-04-01 14:46:41 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,36 @@
+/*************************\
+          Imports
+\*************************/
+
+import {Tokenizer} from "https://fireyauto.github.io/Epoxy/src/tokenizer.js";
+import {AST} from "https://fireyauto.github.io/Epoxy/src/ast.js";
+import {Interpreter} from "https://fireyauto.github.io/Epoxy/src/interpreter.js";
+
+/*************************\
+       Main Function
+\*************************/
+
+function Main(Code,Environment={}){
+	let Result = {Success:false};
+	try {
+		let TStack = Tokenizer.New(Code);
+		TStack.Parse();
+		let AStack = AST.New(TStack);
+		AStack.Parse();
+		let IStack = Interpreter.New(AStack,Environment);
+		IStack.ParseState(IStack.MainState);
+		Result.Success = true,
+			Result.TStack=TStack,
+			Result.AStack=AStack,
+			Result.IStack=IStack;
+	}catch(Result){
+		Result.Success=false,
+			Result.Error=Result;
+	}
+}
+
+/*************************\
+          Exports
+\*************************/
+
+export {Main as RunEpoxy};
