34da95c796c19a1654bf40db0a5c5af46418f0f8
FIREYAUTO <56837610+FIREYAUTO@users.noreply.github.com>
2022-05-05 07:41:32 -0700

    Update library.js

--- library.js
+++ library.js
@@ -14,27 +14,27 @@ class Thread {
 			Yield:false,
 		};
 		this.GetState = function(Stk,St){
-			if(Stk===Stack&&St===State)return null;
+			if(Stk instanceof Stack.constructor&&St instanceof State.constructor)return null;
 			return Hidden.State;
 		}
 		this.GetResolve = function(Stk,St){
-			if(Stk===Stack&&St===State)return null;
+			if(Stk instanceof Stack.constructor&&St instanceof State.constructor)return null;
 			return Hidden.Resolve;
 		}
 		this.WriteResolve = function(Stk,St){
 			if(Hidden.Yield===true)return null;
-			if(Stk===Stack&&St===State)return null;
+			if(Stk instanceof Stack.constructor&&St instanceof State.constructor)return null;
 			Hidden.Resolve=Stk;
 		}
 		this.Yield = function(Stk,St){
 			if(Hidden.Yield===true)return null;
-			if(Stk===Stack&&St===State)return null;
+			if(Stk instanceof Stack.constructor&&St instanceof State.constructor)return null;
 			Hidden.Yield=true;
 			return true;
 		}
 		this.Resume = function(Stk,St,...Arguments){
 			if(!Hidden.Yield)return null;
-			if(Stk===Stack&&St===State)return null;
+			if(Stk instanceof Stack.constructor&&St instanceof State.constructor)return null;
 			Hidden.Yield=false;
 			Hidden.Resolve(...[Stk,St,...Arguments]);
 		}
@@ -43,7 +43,7 @@ class Thread {
 		}
 	}
 	toString(){
-		return "thread"	
+		return "[Epoxy Thread]";	
 	}
 }
 
@@ -56,130 +56,132 @@ const Library = {
 	log:function(Stack,State,...a){
 		console.log(...a);
 	},
-    warn:function(Stack,State,...a){
-    	console.warn(...a);
-    },
-    error:function(Stack,State,...a){
-		console.err(...a);
-    	throw Error(...a);
-    },
-    wait:async function(Stack,State,Time=1){
-    	return await new Promise(r=>setTimeout(r,Time*1000))
-    },
-    type:function(Stack,State,x){
-    	let t=typeof x;
-        if(t==="object"&&x instanceof Array)return"array";
-        if(x===null||x===undefined)return"null";
-        return t;
-    },
-    time:(Stack,State)=>Date.now()/1000,
-    getenv:function(Stack,State,Up=0){
-    	let SearchState = State,AccessPoint = "__CACHED_ENV";
-    	while(Up>0){
-        	let Upper = SearchState.Parent;
-            if(!Upper)break;
-            SearchState = Upper;
-        	Up--;
-        }
-        if(!SearchState)return {};
-        if(SearchState[AccessPoint])return SearchState[AccessPoint];
-        let Access = new Proxy({},{
-        	get:function(self,Name){
-            	return SearchState.GetVariable(Name);
-            },
-            set:function(self,Name,Value){
-            	SearchState.SetVariable(Name,Value);
-            	return Value;
-            }
-        });
-        SearchState[AccessPoint]=Access;
-        return Access;
-    },
-    async:(Stack,State,c,...a)=>{
-    	setTimeout(()=>c(Stack,State,...a),0);
-    },
-    tostring:(Stack,State,a)=>String(a),
-    tofloat:(Stack,State,a)=>parseFloat(a),
-    toint:(Stack,State,a)=>parseInt(a),
-    //{{ Libraries }}\\
-    string:{
-    	split:function(Stack,State,a,b){
-        	return a.split(b||"");
-        },
-        repeat:function(Stack,State,a,b){
-        	return a.repeat(b);
-        },
-        starts:function(Stack,State,a,b){
-        	return a.startsWith(b);
-        },
-        ends:function(Stack,State,a,b){
-        	return a.endsWith(b);
-        },
-        sub:function(Stack,State,a,b,c){
-        	return a.substring(b,c);
-        },
-        subs:function(Stack,State,a,b,c){
-        	return a.substr(b,c);
-        },
-        match:function(Stack,State,a,b){
-        	return a.match(b);
-        },
-        reverse:function(Stack,State,a,b){
-        	return a.split("").reverse().join("");
-        },
-        lower:function(Stack,State,a){
-        	return a.toLowerCase();
-        },
-        upper:function(Stack,State,a){
-        	return a.toUpperCase();
-        },
-        char:function(Stack,State,a){
-        	return a.charCodeAt(0);
-        },
-        byte:function(Stack,State,a){
-        	return String.fromCharCode(a);
-        },
-    },
-    array:{
-    	has:function(Stack,State,a,b){
-        	return a.includes(b);
-        },
-        find:function(Stack,State,a,b){
-        	return a.indexOf(b);
-        },
-        reverse:function(Stack,State,a){
-        	return a.reverse();
-        },
-        join:function(Stack,State,a,b){
-        	return a.join(b||"");
-        },
-        append:function(Stack,State,a,b){
-        	return a.push(b);
-        },
-        prepend:function(Stack,State,a,b){
-        	return a.unshift(b);
-        },
-        remove:function(Stack,State,a,b){
-        	return a.splice(b,1);
-        },
-        pop:function(Stack,State,a){
-        	return a.pop();
-        },
-        prepop:function(Stack,State,a){
-        	return a.shift();
-        },
-        each:function(Stack,State,a,c){
-        	for(let k in a)c(a[k],k,a);
-        },
-    },
-    bit:{
-    	and:(sk,st,a,b)=>a&b,
-        or:(sk,st,a,b)=>a|b,
-        xor:(sk,st,a,b)=>a^b,
-        not:(sk,st,a)=>~a,
-       	rshift:(sk,st,a,b)=>a>>b,
-        lshift:(sk,st,a,b)=>a<<b,
-    },
+	warn:function(Stack,State,...a){
+		console.warn(...a);
+	},
+	error:function(Stack,State,...a){
+		throw Error(...a);
+	},
+	wait:async function(Stack,State,Time=1){
+		return await new Promise(r=>setTimeout(r,Time*1000))
+	},
+	type:function(Stack,State,x){
+		let t=typeof x;
+		if(t==="object"&&x instanceof Array)return"array";
+		if(x===null||x===undefined)return"null";
+		return t;
+	},
+	time:(Stack,State)=>Date.now()/1000,
+	getenv:function(Stack,State,Up=0){
+		let SearchState = State,AccessPoint = "__CACHED_ENV";
+		while(Up>0){
+			let Upper = SearchState.Parent;
+			if(!Upper)break;
+			SearchState = Upper;
+			Up--;
+		}
+		if(!SearchState)return {};
+		if(SearchState[AccessPoint])return SearchState[AccessPoint];
+		let Access = new Proxy({},{
+			get:function(self,Name){
+				return SearchState.GetVariable(Name);
+			},
+			set:function(self,Name,Value){
+				SearchState.SetVariable(Name,Value);
+				return Value;
+			}
+		});
+		SearchState[AccessPoint]=Access;
+		return Access;
+	},
+	async:(Stack,State,c,...a)=>{
+		setTimeout(()=>c(Stack,State,...a),0);
+	},
+	tostring:(Stack,State,a)=>String(a),
+	tofloat:(Stack,State,a)=>parseFloat(a),
+	toint:(Stack,State,a)=>parseInt(a),
+	assert:(Stack,State,c,m)=>{
+		if(!c)Library.error(Stack,State,m);	
+	},
+	//{{ Libraries }}\\
+	string:{
+		split:function(Stack,State,a,b){
+			return a.split(b||"");
+		},
+		repeat:function(Stack,State,a,b){
+			return a.repeat(b);
+		},
+		starts:function(Stack,State,a,b){
+			return a.startsWith(b);
+		},
+		ends:function(Stack,State,a,b){
+			return a.endsWith(b);
+		},
+		sub:function(Stack,State,a,b,c){
+			return a.substring(b,c);
+		},
+		subs:function(Stack,State,a,b,c){
+			return a.substr(b,c);
+		},
+		match:function(Stack,State,a,b){
+			return a.match(b);
+		},
+		reverse:function(Stack,State,a,b){
+			return a.split("").reverse().join("");
+		},
+		lower:function(Stack,State,a){
+			return a.toLowerCase();
+		},
+		upper:function(Stack,State,a){
+			return a.toUpperCase();
+		},
+		char:function(Stack,State,a){
+			return String.fromCharCode(a);
+		},
+		byte:function(Stack,State,a){
+			return a.charCodeAt(0);
+		},
+	},
+	array:{
+		has:function(Stack,State,a,b){
+			return a.includes(b);
+		},
+		find:function(Stack,State,a,b){
+			return a.indexOf(b);
+		},
+		reverse:function(Stack,State,a){
+			return a.reverse();
+		},
+		join:function(Stack,State,a,b){
+			return a.join(b||"");
+		},
+		append:function(Stack,State,a,b){
+			return a.push(b);
+		},
+		prepend:function(Stack,State,a,b){
+			return a.unshift(b);
+		},
+		remove:function(Stack,State,a,b){
+			return a.splice(b,1);
+		},
+		pop:function(Stack,State,a){
+			return a.pop();
+		},
+		prepop:function(Stack,State,a){
+			return a.shift();
+		},
+		each:function(Stack,State,a,c){
+			for(let k in a)c(a[k],k,a);
+		},
+	},
+	bit:{
+		and:(sk,st,a,b)=>a&b,
+		or:(sk,st,a,b)=>a|b,
+		xor:(sk,st,a,b)=>a^b,
+		not:(sk,st,a)=>~a,
+		rshift:(sk,st,a,b)=>a>>b,
+		lshift:(sk,st,a,b)=>a<<b,
+	},
 	thread:{
 		get:async function(Stack,State){
 			let T = State.GlobalRead("Thread");
@@ -244,7 +246,36 @@ const Library = {
 		pi:Math.PI,
 		e:Math.E,
 	},
-	
+	json:{
+		encode:(stk,s,x)=>JSON.stringify(x),
+		decode:(stk,s,x)=>JSON.parse(x),
+	},
+	regex:{
+		new:(stk,s,x,i)=>new RegExp(x,i)	
+	},
+	debug:{
+		varlen:(stk,s)=>s.Variables.length,
+		gvarlen:(stk,s)=>s.GetGlobalVariables().length,
+		newvar:(stk,s,n,v,c=false)=>{
+			if(typeof n!="string")return;
+			s.NewVariable(n,v,{
+				Constant:c,	
+			});
+			return v;
+		},
+		delvar:(stk,s,n,l=false)=>{
+			if(typeof n!="string")return;
+			s.DeleteVariable(n,l);
+		},
+		getvar:(stk,s,n)=>{
+			return s.GetVariable(n);
+		},
+		read:(stk,s,n)=>{
+			let r=s.Read(n);
+			if(r===undefined)r=null;
+			return r;
+		},
+	},
 };
 
 Library.env = Library;
