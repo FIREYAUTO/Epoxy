c4cf73721e7ec4a6bf8f8b6a285c9519b04ea6bd
FIREYAUTO <56837610+FIREYAUTO@users.noreply.github.com>
2022-05-16 07:21:29 -0700

    Create defaultenvironment.js

---
+++ src/defaultenvironment.js
@@ -0,0 +1,13 @@
+/*************************\
+    Default Environment
+\*************************/
+
+const DefaultEnvironment = {
+	"_version":"Epoxy 1.2",
+};
+
+/*************************\
+          Exports
+\*************************/
+
+export {DefaultEnvironment};
