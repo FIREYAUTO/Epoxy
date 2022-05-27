89adaca8207df7932c15577ebbfe5b6090dae536
FIREYAUTO <56837610+FIREYAUTO@users.noreply.github.com>
2022-05-27 07:08:52 -0700

    Update tokens.js

--- src/tokens.js
+++ src/tokens.js
@@ -115,6 +115,7 @@ const RawTokens = [
 	new InternalToken("COMMA",",",Types[3]),
 	new InternalToken("LENGTH","#",Types[3]),
 	new InternalToken("SELFCALL",">>",Types[3]),
+	new InternalToken("ASSIGNMENT","=",Types[3]),
 	//Bracket Tokens
 	new InternalToken("BOPEN","{",Types[4]),
 	new InternalToken("BCLOSE","}",Types[4]),
